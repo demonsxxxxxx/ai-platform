fdd8aa1b0e4be68f949b0517a6896a23c2151c82
Xinlin.jiang <1724308276@qq.com>
2026-09-08 10:58:40 +0800

    修正冻结 Repository bridge 的变更边界

--- tests/test_admin_run_detail.py
+++ tests/test_admin_run_detail.py
@@ -315,6 +315,7 @@ def test_admin_run_detail_returns_explainability_contract(monkeypatch):
                     "used": True,
                     "usage": {
                         "used_skills_source": "executor_hook",
+                        "inferred_used": False,
                     },
                     "created_at": None,
                 }
